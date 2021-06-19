8f8a832c29fc378973f81facbd5c6c37033a6761
Nandini Agarwal <nia78831@gmail.com>
2021-06-19 18:00:23 +0530

    class components

--- basics/src/App.js
+++ basics/src/App.js
@@ -2,12 +2,14 @@ import React , { Component } from 'react'
 //import logo from './logo.svg';
 import './App.css';
 import Hello from './components/Hello';
+import Classcomp from './components/Classcomp';
 
  class App extends Component {
    render() {
   return (
     <div className="App">
       <Hello />
+      <Classcomp />
     </div>
   );
 }
---
+++ basics/src/components/Classcomp.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+
+class Classcomp extends Component {
+    render() {
+        return <h1>Class Components</h1>
+    }
+}
+
+
+export default Classcomp;
