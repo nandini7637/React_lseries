1cfe02214ff5cbf27e7c1af879d09ca259074804
Nandini Agarwal <nia78831@gmail.com>
2021-06-19 12:12:35 +0530

    functional component

---
+++ basics/src/components/Hello.js
@@ -0,0 +1,18 @@
+
+/*Functional Components*/
+
+import React from 'react'
+
+/*Method for function type 1*/
+
+/*function Hello(){
+    return <h1>Hola Fellow Developer👋🏻</h1>
+}*/
+
+// or
+
+// Type 2 using arrow function notation
+
+const Hello = () => <h1>Hola Fellow Developer👋🏻</h1>
+
+export default Hello;
