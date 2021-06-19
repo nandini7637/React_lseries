bba04379170f6548a26285aeed2fddbccbdcac34
Nandini Agarwal <nia78831@gmail.com>
2021-06-19 18:06:01 +0530

    class comp h4 in h1

--- basics/src/components/Classcomp.js
+++ basics/src/components/Classcomp.js
@@ -2,8 +2,8 @@ import React, {Component} from 'react'
 
 class Classcomp extends Component {
     render() {
-        return <h1>Class Components</h1>
-    }
+        return <h1>React Basics <br /> <h4>Class Components</h4></h1> 
+      }
 }
 
 
